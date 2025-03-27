697c232768645b488db271adf4a6de787e462082
Jason Antwi-Appah <hey@jasonaa.me>
2025-03-27 14:00:29 -0500

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,12 +13,10 @@ set -gx PYENV_ROOT "$HOME/.pyenv"
 set -gx GPG_TTY (tty)
 set -gx EDITOR "micro"
 set -gx HOMEBREW_BUNDLE_FILE "$HOME/Brewfile"
-set -gx CLOUDFLARE_API_TOKEN "MgF3xW6NoTqBRPIaqlHBry3-veU_mRMzU1BTQRHW"
 set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
 
 
 # bun
-set --export BUN_INSTALL "$HOME/.bun"
 fish_add_path --prepend "$HOME/.bun/bin"
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
--- .config/fish/functions/pm-patch.fish
+++ .config/fish/functions/pm-patch.fish
@@ -1,5 +1,5 @@
 function pm-patch --description "Patch Portland Mono fonts with Nerd Fonts"
-    for i in ~/library/fonts/PortlandMono-*
+    for i in ~/library/fonts/Monaspace*
         fontforge -script ~/nerd-fonts/font-patcher -c $i --careful -l=false
     end
 end 
